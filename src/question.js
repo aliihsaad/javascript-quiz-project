8dc7a720d15fad46287d51a8206bf3843d87da28
Ali Saad <59366979+aliihsaad@users.noreply.github.com>
2026-03-28 16:43:00 +0100

    day-1

--- src/question.js
+++ src/question.js
@@ -1,7 +1,23 @@
 class Question {
     // YOUR CODE HERE:
+      constructor(text, choices, answer, difficulty) {
+    // Save each argument as a property on the object
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+    this.difficulty = difficulty;
+      }
     //
     // 1. constructor (text, choices, answer, difficulty)
 
     // 2. shuffleChoices()
-}
+      shuffleChoices() {
+        for (let i = this.choices.length - 1; i > 0; i--) {
+           let j = Math.floor(Math.random() * (i + 1));
+           let temp = this.choices[i];       // save choices[i] before it gets overwritten
+           this.choices[i] = this.choices[j]; // choices[i] is now overwritten — but temp has the old value
+           this.choices[j] = temp;           // restore the old choices[i] into choices[j]
+        }
+      }
+
+    }
